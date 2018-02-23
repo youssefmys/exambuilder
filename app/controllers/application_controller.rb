d7ce0d31d126b657c26b9ac12d2cd8a67bb33029
youssefmys <mosa500@gmail.com>
2018-02-23 17:15:53 +0200

    redirect home page if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :authenticate_user! , :logged_in?, :authenticate_user, :sign_in, :sign_out
+  helper_method :current_user, :authenticate_user! , :logged_in?, :authenticate_user, :sign_in, :sign_out, :redirect_if_logged_in
 
 
   def authenticate_user!
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :redirect_if_logged_in
   layout  'main_layout'
 
   def home
+
     render :home
   end
 
